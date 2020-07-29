87f5f41f61fd3fdc57d81e5bea4d4d30e96723c5
Rob Birch <rbirchtree@gmail.com>
2020-07-29 13:04:39 -0700

    corrected data date

--- sqltable.sql
+++ sqltable.sql
@@ -23,35 +23,36 @@ CREATE TABLE userdata (
 INSERT INTO USERDATA (dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours, timeDrivingtoRehabPerAVisitRndTrip, 
 timeWithAtty, timeAtAccident, timeRentingACar, timeDoingRehabPerADay, dateRehabIsFinished, hourlyWageForOccupation ,occupation,
 zipCodeOfAccident, insurance, age, settlementAmt,notes)
-VALUES ('02/1/2014', 60, 1,1,1,2,1,1,'05/1/14',8,'pizza delivery driver',78738, 'ALL-STATE', 28,14000,'Victim was stopped at redlight and took full impact from behind'); 
+VALUES ('02/1/2014', 60, 1,1,1,2,1,1,'05/1/14',8,'Pizza Delivery Driver',78738, 'ALL-STATE', 28,14000,'Victim was stopped at redlight and took full impact from behind'); 
 
 
 INSERT INTO USERDATA (dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours, timeDrivingtoRehabPerAVisitRndTrip, 
 timeWithAtty, timeAtAccident, timeRentingACar, timeDoingRehabPerADay, dateRehabIsFinished, hourlyWageForOccupation ,occupation,
 zipCodeOfAccident, insurance, age, settlementAmt,notes)
-VALUES ('12/18/2018', 16, 1,1,1,2,1,1,'05/1/14',25,'home health',78738, 'ACME', 35, 4400,'She was hit by a truck turning left'); 
+VALUES ('12/18/2018', 16, 1,1,1,2,1,1,'05/1/14',25,'Home Health',78738, 'ACME', 35, 4400,'She was hit by a truck turning left'); 
 
 INSERT INTO USERDATA (dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours, timeDrivingtoRehabPerAVisitRndTrip, 
 timeWithAtty, timeAtAccident, timeRentingACar, timeDoingRehabPerADay, dateRehabIsFinished, hourlyWageForOccupation ,occupation,
 zipCodeOfAccident, insurance, age, settlementAmt,notes)
-VALUES ('4/14/2018', 16, 1,1,1,2,1,1,'04/14/18',8,'retired',78634, 'ACME', 83, 5000,'He was hit by a van while parked'); 
+VALUES ('4/14/2018', 16, 1,1,1,2,1,1,'04/14/18',8,'Retired',78634, 'ACME', 83, 5000,'He was hit by a van while parked'); 
 
 INSERT INTO USERDATA (dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours, timeDrivingtoRehabPerAVisitRndTrip, 
 timeWithAtty, timeAtAccident, timeRentingACar, timeDoingRehabPerADay, dateRehabIsFinished, hourlyWageForOccupation ,occupation,
 zipCodeOfAccident, insurance, age, settlementAmt,notes)
-VALUES ('4/21/2019', 32, 2,1,1,1,1,1,'07/01/18',24,'construction',78753, 'ACME', 21, 9700,'He was hit by a truck while driving'); 
+VALUES ('4/21/2019', 32, 2,1,1,1,1,1,'07/01/19',24,'Construction',78753, 'ACME', 21, 9700,'He was hit by a truck while driving'); 
 
 INSERT INTO USERDATA (dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours, timeDrivingtoRehabPerAVisitRndTrip, 
 timeWithAtty, timeAtAccident, timeRentingACar, timeDoingRehabPerADay, dateRehabIsFinished, hourlyWageForOccupation ,occupation,
 zipCodeOfAccident, insurance, age, settlementAmt,notes)
-VALUES ('9/24/2018', 34, 2,1,1,1,1,1,'12/31/18',32,'student',78744, 'ACME', 19, 15000,'He was a passenger hit by a truck'); 
+VALUES ('9/24/2018', 34, 2,1,1,1,1,1,'12/31/18',32,'Student',78744, 'ACME', 19, 15000,'He was a passenger hit by a truck'); 
 
 INSERT INTO USERDATA (dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours, timeDrivingtoRehabPerAVisitRndTrip, 
 timeWithAtty, timeAtAccident, timeRentingACar, timeDoingRehabPerADay, dateRehabIsFinished, hourlyWageForOccupation ,occupation,
 zipCodeOfAccident, insurance, age, settlementAmt,notes)
-VALUES ('1/23/2019', 12, 2,1,1,1,1,1,'4/12/19',30,'retail',78641, 'ACME', 19, 7500,'She was a passenger hit by a motorcycle'); 
+VALUES ('1/23/2019', 12, 2,1,1,1,1,1,'4/12/19',30,'Retail',78641, 'ACME', 19, 7500,'She was a passenger hit by a motorcycle'); 
 
 INSERT INTO USERDATA (dateOfAccident, numOfVisitsToRehab ,timeAtRehabInHours, timeDrivingtoRehabPerAVisitRndTrip, 
 timeWithAtty, timeAtAccident, timeRentingACar, timeDoingRehabPerADay, dateRehabIsFinished, hourlyWageForOccupation ,occupation,
 zipCodeOfAccident, insurance, age, settlementAmt,notes)
-VALUES ('5/14/17', 5, 8,1,1,1,1,1,'6/1/17',70,'Mechanical Engineer',9000, 'ACME', 33, 9000,'He was a driving a Prius and the driver ran away'); 
+VALUES ('5/14/17', 5, 8,1,1,1,1,1,'6/1/17',70,'Mechanical Engineer',94304, 'ACME', 33, 9000,'He was a driving a Prius and the driver ran away, filed claim later'); 
+
